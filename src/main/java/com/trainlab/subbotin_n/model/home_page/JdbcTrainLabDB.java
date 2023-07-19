0c3905703a61d0c9fe331c36bb58e0d0609bdd6c
subbotin-n-o <subbotin.n.o@gmail.com>
2023-07-20 00:21:30 +0400

    #7 added jdbc-ssh TL-70

--- src/main/java/com/trainlab/subbotin_n/model/home_page/JdbcTrainLabDB.java
+++ src/main/java/com/trainlab/subbotin_n/model/home_page/JdbcTrainLabDB.java
@@ -1,5 +1,9 @@
 package com.trainlab.subbotin_n.model.home_page;
 
+import com.m11n.jdbc.ssh.SshConfiguration;
+import com.m11n.jdbc.ssh.SshDriver;
+import com.m11n.jdbc.ssh.SshTunnel;
+
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.Statement;
@@ -7,10 +11,22 @@ import java.sql.ResultSet;
 
 public class JdbcTrainLabDB {
     public static void main(String[] args) throws Exception {
-       Connection connection = DriverManager.getConnection(
-               "jdbc:postgresql://tldb-postgresql-fra1-81116-do-user-14346310-0.b.db.ondigitalocean.com:25060/trainlab",
-               "trainlab",
-               "AVNS_yXtS-TsJq6YJmCAe19y");
+
+
+//        SshConfiguration sshConfiguration = new SshConfiguration();
+//
+//        SshDriver sshDriver = new SshDriver();
+//
+//        SshTunnel sshTunnel = new SshTunnel(sshConfiguration);
+//       Connection connection = DriverManager.getConnection(
+//               "jdbc:postgresql://tldb-postgresql-fra1-81116-do-user-14346310-0.b.db.ondigitalocean.com:25060/trainlab",
+//               "trainlab",
+//               "AVNS_yXtS-TsJq6YJmCAe19y");
+
+        Connection connection = DriverManager.getConnection(
+                "jdbc:postgresql://tldb-postgresql-fra1-81116-do-user-14346310-0.b.db.ondigitalocean.com:25060/trainlab",
+                "trainlab",
+                "AVNS_yXtS-TsJq6YJmCAe19y");
 
        Statement statement = connection.createStatement();
        ResultSet results = statement.executeQuery("SELECT * FROM frontend_data");
