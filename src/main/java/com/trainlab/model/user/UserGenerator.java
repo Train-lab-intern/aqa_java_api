6004d6670cc87a50117ea829dcd0a30760b70975
subbotin-n-o <subbotin.n.o@gmail.com>
2023-08-23 23:08:12 +0400

    update UserGenerator.java

--- src/main/java/com/trainlab/model/user/UserGenerator.java
+++ src/main/java/com/trainlab/model/user/UserGenerator.java
@@ -7,6 +7,30 @@ import java.util.Locale;
 public class UserGenerator {
 
     private static final Faker faker = new Faker(new Locale("en"));
+    private static final User user = new User();
+
+    public static User getUser(UserType userType) {
+
+        switch (userType) {
+            case VALID_USER:
+                user.setUsername(faker.name().firstName());
+                user.setEmail(faker.internet().emailAddress());
+                user.setPassword(faker.internet().password(8, 10, true, false, true));
+                break;
+            case NO_NAME_USER:
+                user.setEmail(faker.internet().emailAddress());
+                user.setPassword(faker.internet().password(8, 10, true, false, true));
+                break;
+            case NO_EMAIL_USER:
+                user.setUsername(faker.name().firstName());
+                user.setPassword(faker.internet().password(8, 10, true, false, true));
+                break;
+            case NO_PASSWORD_USER:
+                user.setUsername(faker.name().firstName());
+                break;
+        }
+        return user;
+    }
 
     public static String getRandomUserName() {
         return faker.name().firstName();
