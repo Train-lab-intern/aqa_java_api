450b43c01b20c4a4688cd2a29e44019405c9c179
subbotin-n-o <subbotin.n.o@gmail.com>
2023-08-29 23:17:55 +0400

    added completeRegistration() in UserClient.java

--- src/main/java/com/trainlab/model/user/UserClient.java
+++ src/main/java/com/trainlab/model/user/UserClient.java
@@ -8,7 +8,8 @@ import static io.restassured.RestAssured.given;
 public class UserClient extends Client {
 
     private static final String PATH_REGISTER = "/api/v1/users/register";
-    private static final String LOGIN_REGISTER = "/api/v1/auth";
+    private static final String PATH_LOGIN_REGISTER = "/api/v1/auth";
+    private static final String PATH_COMPLETE_REGISTER = "/api/v1/users/complete-registration";
 
     public static ValidatableResponse createUser(User user) {
 
@@ -27,9 +28,16 @@ public class UserClient extends Client {
                 .spec(getSpec())
                 .body(credentials)
                 .when()
-                .post(LOGIN_REGISTER)
+                .post(PATH_LOGIN_REGISTER)
                 .then();
 
+    }
+
+    public static ValidatableResponse completeRegistration(String email) {
+        return given()
+                .spec(getSpec())
+                .get(String.format(PATH_COMPLETE_REGISTER + "?userEmail=%s", email))
+                .then();
     }
 
 }
